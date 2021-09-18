bdf6976b4f5cbd52a27b610f22bb0f8a258a9c8b
Charlie Laver <charlielaver7@gmail.com>
2021-09-18 16:17:38 +0100

    camera follow player

---
+++ Assets/Scripts/Player/cameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    private Vector3 camOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    void Start()
+    {
+        camOffset = transform.position - PlayerTransform.position;
+    }
+
+    void Update()
+    {
+        Vector3 newPos = PlayerTransform.position + camOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
